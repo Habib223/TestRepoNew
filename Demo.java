a67d21c5b5b374baefb3d3286421f9da4027360b
dev2 <habib@gmail.com>
2021-01-25 13:51:14 +0530

    this im adding substraction  in the new vairable..

--- Demo.java
+++ Demo.java
@@ -9,6 +9,8 @@ public class Demo{
      int c=a+b;
      System.out.println(c);
      System.out.println("Addition...................");
+     int d=a*b
+     System.out.println("substraction.."+d);
      }
 
 
